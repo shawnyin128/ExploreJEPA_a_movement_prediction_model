bf41cd747ece8ccfb93f60ca231e116317917517
shawnyin <yxyoung128@gmail.com>
2025-04-22 10:59:42 -0400

    [model]: fix bug

--- model/encoder.py
+++ model/encoder.py
@@ -28,14 +28,16 @@ class StateEncoder(nn.Module):
 
         agent_rep = self.agent_pwConv(agent_state) # [b, 16, 65, 65]
         agent_rep = self.agent_blocks(agent_rep) # [b, 16, 65, 65]
-        agent_rep = self.gap(agent_rep) # [b, 576]
+        agent_rep = self.gap(agent_rep)
+        agent_rep = agent_rep.view(agent_rep.size(0), -1) # [b, 576]
         agent_rep = self.dropout(agent_rep)
         agent_rep = self.agent_fc(agent_rep) # [b, 256]
 
         env_rep = self.env_pwConv(env_state) # [b, 16, 65, 65]
         env_rep = self.env_blocks(env_rep) # [b, 16, 65, 65]
-        env_rep = self.gap(env_rep) # [b, 576]
-        env_rep = self.drop(env_rep)
+        env_rep = self.gap(env_rep)
+        env_rep = env_rep.view(env_rep.size(0), -1) # [b, 576]
+        env_rep = self.dropout(env_rep)
         env_rep = self.env_fc(env_rep) # [b, 256]
 
         rep = agent_rep+env_rep # [b, 256]
