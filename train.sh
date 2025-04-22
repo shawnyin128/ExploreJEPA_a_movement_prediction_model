1933635dafcec58350fbfc4a49d8093d747258f0
shawnyin <yxyoung128@gmail.com>
2025-04-22 12:26:51 -0400

    [gitignore]: remove some files

--- train.sh
+++
@@ -1,6 +0,0 @@
-singularity exec --bind /scratch --nv --overlay /scratch/xy2053/overlay-25GB-500K.ext3:rw /scratch/xy2053/ubuntu-20.04.3.sif /bin/bash -c "
-source /ext3/miniconda3/etc/profile.d/conda.sh
-conda activate dl_base
-cd /scratch/xy2053/2025SP/2572_DeepLearning/codes/
-python ./train.py
-"
